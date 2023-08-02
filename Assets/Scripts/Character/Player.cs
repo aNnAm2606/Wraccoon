e2f54a82b0d81f847459445f0a9300ddfdc5eb76
aNnAm2606 <73582949+aNnAm2606@users.noreply.github.com>
2023-08-02 14:27:16 +0200

    Raycasting + interaction with sofas

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -8,14 +8,15 @@ public class Player : MonoBehaviour
     [SerializeField] public float speed = 7f;
     [SerializeField] public float jumpSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask objectsLayerMask;
 
     private bool isWalking;
     private bool isJumping;
 
     Vector3 movementVelocity;
     Vector3 turnVelocity;
+    private Vector3 lastInteractDirection; 
 
-    
     public float rotationSpeed = 100f;
     private float gravityValue = -9.81f;
 
@@ -28,12 +29,51 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    public bool IsJumping()
+    {
+        return isJumping;
+    }
+
+    private void HandleInteractions()
+    {
+        Vector3 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 direction = new Vector3(inputVector.x, inputVector.y, inputVector.z).normalized;
+
+        float interactDistance = 2f;
+
+        if(direction != Vector3.zero)
+        {
+            lastInteractDirection = direction;
+        }
+
+       if (Physics.Raycast(transform.position, lastInteractDirection, out RaycastHit raycastHit, interactDistance, objectsLayerMask))
+        {
+          if(raycastHit.transform.TryGetComponent(out Sofa sofa))
+            {
+                //has sofa
+                sofa.Interact();
+            }
+        }
+    }
+
+    private void HandleMovement()
+    {
         isJumping = false;
 
         Vector3 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 direction = new Vector3(inputVector.x, inputVector.y, inputVector.z).normalized;
-        
-        if (characterController.isGrounded) 
+
+        if (characterController.isGrounded)
         {
             movementVelocity = transform.forward * speed * inputVector.z;
             turnVelocity = transform.up * rotationSpeed * inputVector.x;
@@ -47,20 +87,10 @@ public class Player : MonoBehaviour
 
         //Add gravity
         movementVelocity.y += gravityValue * Time.deltaTime;
-        characterController.Move(movementVelocity*Time.deltaTime);
+        characterController.Move(movementVelocity * Time.deltaTime);
         transform.Rotate(turnVelocity * Time.deltaTime);
 
         isWalking = direction != Vector3.zero;
-     
-    }
 
-    public bool IsWalking()
-    {
-        return isWalking;
-    }
-
-    public bool IsJumping()
-    {
-        return isJumping;
     }
 }
